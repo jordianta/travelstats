effe29e4560a3a1175c25477118847ba8e6f39a6
jordianta <antamack@gmail.com>
2017-11-23 20:08:42 +0100

    - Changed the main

--- src/main/java/com/trebol/travelstats/TravelStatsApplication.java
+++ src/main/java/com/trebol/travelstats/TravelStatsApplication.java
@@ -1,12 +1,26 @@
 package com.trebol.travelstats;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class TravelStatsApplication {
+public class TravelStatsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(TravelStatsApplication.class);
+        SpringApplication.run(TravelStatsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TravelStatsApplication.class);
     }
 }
