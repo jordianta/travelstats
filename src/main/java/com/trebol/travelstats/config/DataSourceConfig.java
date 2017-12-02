4af58b9918f1433441e20a036d62f9c26fcf1b05
jordianta <antamack@gmail.com>
2017-12-02 13:14:11 +0100

    - Moving DataSourceConfig to TravelStatsApplication

--- src/main/java/com/trebol/travelstats/config/DataSourceConfig.java
+++ src/main/java/com/trebol/travelstats/config/DataSourceConfig.java
@@ -8,30 +8,30 @@ import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class DataSourceConfig {
 
-    private static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
-
-    private static final String URL = "jdbc:mysql://10.131.32.66:3306/travelstats";
-    private static final String USERNAME = "userDJ2";
-    private static final String PASSWORD = "sV223KDtaIPDeJ2G";
-
-//    private static final String URL = "jdbc:mysql://localhost:3306/travelstats";
-//    private static final String USERNAME = "root";
-//    private static final String PASSWORD = ")n6QSc7d.D$H[a*t";
-
-
-    @Bean
-    @Primary
-    public DataSource getDataSource() {
-
-        return DataSourceBuilder
-                .create()
-                .url(URL)
-                .username(USERNAME)
-                .password(PASSWORD)
-                .driverClassName(DRIVER_CLASS_NAME)
-                .build();
-    }
+//    private static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+//
+//    private static final String URL = "jdbc:mysql://10.131.32.66:3306/travelstats";
+//    private static final String USERNAME = "userDJ2";
+//    private static final String PASSWORD = "sV223KDtaIPDeJ2G";
+//
+////    private static final String URL = "jdbc:mysql://localhost:3306/travelstats";
+////    private static final String USERNAME = "root";
+////    private static final String PASSWORD = ")n6QSc7d.D$H[a*t";
+//
+//
+//    @Bean
+//    @Primary
+//    public DataSource getDataSource() {
+//
+//        return DataSourceBuilder
+//                .create()
+//                .url(URL)
+//                .username(USERNAME)
+//                .password(PASSWORD)
+//                .driverClassName(DRIVER_CLASS_NAME)
+//                .build();
+//    }
 }
