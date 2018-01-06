640c4a0f8406b3b50d4c0052d0ffe9e07af1e206
jordianta <antamack@gmail.com>
2018-01-06 12:22:54 +0100

    - Modified the way to choose the colors

--- src/main/webapp/static/js/map.js
+++ src/main/webapp/static/js/map.js
@@ -8,32 +8,87 @@ var BUBBLE_COLOR_POINT = "#F845A2";
 var MAXIMUM_NUMBER_OF_ARCHES = 0; // if 0 --> no maximum number of arches
 var FILL_COLOR = "rgb(211, 216, 222)";
 
+function getColorArray(size) {
+
+    var colors = new Array();
+
+    for(var i = 0;i < size;i++) {
+        if(i == 0) {
+            colors[i] = getRandomColor();
+        } else {
+            var previousColor = colors[i - 1];
+            var color;
+            do {
+                color = getRandomColor();
+            } while(!colorIsValid(previousColor, color));
+            colors[i] = color;
+        }
+    }
+    return colors;
+}
+
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
-    var color = '#';
+    var color = "";
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
+
+function colorIsValid(hex1, hex2) {
+    // get red/green/blue int values of hex1
+    var r1 = parseInt(hex1.substring(0, 2), 16);
+    var g1 = parseInt(hex1.substring(2, 4), 16);
+    var b1 = parseInt(hex1.substring(4, 6), 16);
+    // get red/green/blue int values of hex2
+    var r2 = parseInt(hex2.substring(0, 2), 16);
+    var g2 = parseInt(hex2.substring(2, 4), 16);
+    var b2 = parseInt(hex2.substring(4, 6), 16);
+
+    if(colorIsTooDark(r2, g2, b2)) {
+        return false;
+    }
+
+    // calculate differences between reds, greens and blues
+    var r = 255 - Math.abs(r1 - r2);
+    var g = 255 - Math.abs(g1 - g2);
+    var b = 255 - Math.abs(b1 - b2);
+    // limit differences between 0 and 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    // 0 means opposit colors, 1 means same colors
+    return (r + g + b) / 3 < 0.5;
+}
+
+function colorIsTooDark(r, g, b) {
+    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    return luma < 90;
+}
+
+var colorArray = getColorArray(16);
+var index = 0;
+
 var YEARS_COLORS = [
-	{"year":1996, "color":getRandomColor()},
-	{"year":2000, "color":getRandomColor()},
-	{"year":2005, "color":getRandomColor()},
-	{"year":2006, "color":getRandomColor()},
-	{"year":2007, "color":getRandomColor()},
-	{"year":2008, "color":getRandomColor()},
-	{"year":2009, "color":getRandomColor()},
-	{"year":2010, "color":getRandomColor()},
-	{"year":2011, "color":getRandomColor()},
-	{"year":2012, "color":getRandomColor()},
-	{"year":2013, "color":getRandomColor()},
-	{"year":2014, "color":getRandomColor()},
-	{"year":2015, "color":getRandomColor()},
-	{"year":2016, "color":getRandomColor()},
-	{"year":2017, "color":getRandomColor()},
-	{"year":2018, "color":getRandomColor()}
+	{"year":1996, "color": "#" + colorArray[index++]},
+	{"year":2000, "color": "#" + colorArray[index++]},
+	{"year":2005, "color": "#" + colorArray[index++]},
+	{"year":2006, "color": "#" + colorArray[index++]},
+	{"year":2007, "color": "#" + colorArray[index++]},
+	{"year":2008, "color": "#" + colorArray[index++]},
+	{"year":2009, "color": "#" + colorArray[index++]},
+	{"year":2010, "color": "#" + colorArray[index++]},
+	{"year":2011, "color": "#" + colorArray[index++]},
+	{"year":2012, "color": "#" + colorArray[index++]},
+	{"year":2013, "color": "#" + colorArray[index++]},
+	{"year":2014, "color": "#" + colorArray[index++]},
+	{"year":2015, "color": "#" + colorArray[index++]},
+	{"year":2016, "color": "#" + colorArray[index++]},
+	{"year":2017, "color": "#" + colorArray[index++]},
+	{"year":2018, "color": "#" + colorArray[index++]}
 ];
+
 var mapFills = {};
 var countries = {};
 var currentPointers = [];
