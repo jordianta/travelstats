57bc095e61f2e41b5fc883954b9fad5e02ba3d05
Unknown <antamack@gmail.com>
2021-12-24 12:50:01 +0100

    added logs for DB connection

--- src/main/java/com/trebol/travelstats/config/DataSourceConfig.java
+++ src/main/java/com/trebol/travelstats/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.trebol.travelstats.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 @Configuration
+@Slf4j
 public class DataSourceConfig {
 
     private static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
@@ -22,10 +24,14 @@ public class DataSourceConfig {
     public DataSource getDataSource() throws URISyntaxException {
 
         final var dbUri = new URI(System.getenv("JAWSDB_URL"));
+        log.info("dbUri: {}", dbUri);
 
         final var username = dbUri.getUserInfo().split(":")[0];
         final var password = dbUri.getUserInfo().split(":")[1];
         final var dbUrl = "jdbc:mysql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+        log.info("username: {}", username);
+        log.info("password: {}", password);
+        log.info("dbUrl: {}", dbUrl);
 
         return DataSourceBuilder
                 .create()
